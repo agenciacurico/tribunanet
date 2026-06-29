920fd1a3b06579438bc28926ba6a2d1e59ecd95e
Mario Torres <info@agenciacurico.cl>
2026-06-29 11:21:05 -0400

    Ajusta overlay para PRISM iPhone

--- resources/views/livewire/overlay/game-overlay.blade.php
+++ resources/views/livewire/overlay/game-overlay.blade.php
@@ -1,4 +1,4 @@
-<div wire:poll.500ms class="fixed bottom-8 left-1/2 -translate-x-1/2 w-[1200px]">
+<div wire:poll.500ms class="fixed bottom-8 left-1/2 -translate-x-1/2 max-w-[1200px] w-[95vw]">
 
     <div class="bg-slate-900/90 backdrop-blur-xl rounded-2xl shadow-2xl px-8 py-5">
 
@@ -19,8 +19,8 @@
                     <div class="h-8"></div>
                 @endif
 
-                <div class="text-cyan-400 text-8xl font-black leading-none">
-                    {{ str_pad($currentSet->home_score,3,'0',STR_PAD_LEFT) }}
+                <div class="text-cyan-400 text-7xl font-black leading-none">
+                    {{ str_pad($currentSet->home_score, 2, '0', STR_PAD_LEFT) }}
                 </div>
 
             </div>
@@ -69,8 +69,8 @@
                     <div class="h-8"></div>
                 @endif
 
-                <div class="text-red-400 text-8xl font-black leading-none">
-                    {{ str_pad($currentSet->away_score,3,'0',STR_PAD_LEFT) }}
+                <div class="text-red-400 text-7xl font-black leading-none">
+                    {{ str_pad($currentSet->away_score, 2, '0', STR_PAD_LEFT) }}
                 </div>
 
             </div>
